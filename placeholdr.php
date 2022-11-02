d9fcddda5852968354eec804e601c90567bfbd78
samuel424 <samuel.50@live.se>
2022-11-02 09:05:39 +0100

    Add files via upload

---
+++ placeholdr.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Åquaman</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <?php include "standardAssets.php"; pageheader();?>
+    
+    <!--Content-->
+    <div class="body">
+        
+    </div> <!--/page-->
+
+    <?php pagefooter();?>
+</body>
+</html>
